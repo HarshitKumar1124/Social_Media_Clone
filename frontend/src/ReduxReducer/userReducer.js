f88c42185ed3b3de19f5a93f84a8cc7d8a3d6858
Harshit Kumar <83143382+HarshitKumar1124@users.noreply.github.com>
2024-10-17 15:15:44 +0530

    Update userReducer.js Commented

--- frontend/src/ReduxReducer/userReducer.js
+++ frontend/src/ReduxReducer/userReducer.js
@@ -1,93 +1,93 @@
-import {
+// import {
 
-    REQUEST_LOAD_USER,
-   SUCCESS_LOAD_USER,
-    FAIL_LOAD_USER,
-    REQUEST_LOGOUT_USER,
-    SUCCESS_LOGOUT_USER,
-     FAIL_LOGOUT_USER,
+//     REQUEST_LOAD_USER,
+//    SUCCESS_LOAD_USER,
+//     FAIL_LOAD_USER,
+//     REQUEST_LOGOUT_USER,
+//     SUCCESS_LOGOUT_USER,
+//      FAIL_LOGOUT_USER,
    
-     REQUEST_USER_LOGIN,
-     SUCCESS_USER_LOGIN,
-      FAIL_USER_LOGIN,
+//      REQUEST_USER_LOGIN,
+//      SUCCESS_USER_LOGIN,
+//       FAIL_USER_LOGIN,
 
-      REQUEST_ALL_USER,
-    SUCCESS_ALL_USER,
-     FAIL_ALL_USER,
+//       REQUEST_ALL_USER,
+//     SUCCESS_ALL_USER,
+//      FAIL_ALL_USER,
 
-} from "../ReduxConstants/userConstants"
-
-
-
-// User Information
-
-export const User = (state={},action)=>{
-
-    switch(action.type)
-    {
-        case REQUEST_LOAD_USER:
-        case REQUEST_USER_LOGIN:
-        case REQUEST_LOGOUT_USER:
-            return{
-                loading:true,
-
-            };
-        case SUCCESS_LOAD_USER:
-        case SUCCESS_USER_LOGIN:
-        case SUCCESS_LOGOUT_USER:
-            return{
-                loading:false,
-                isAuth:action.payload.authStatus,
-                user:action.payload.user,
-                message:action.payload.message
-            }
-        case FAIL_LOAD_USER: 
-        case FAIL_USER_LOGIN:
-        case FAIL_LOGOUT_USER: 
-            return{
-                loading:false,
-                isAuth:false,
-                user:null,
-                message:action.payload
-
-            }
-        default:
-            return state
-    }
-}
+// } from "../ReduxConstants/userConstants"
 
 
 
+// // User Information
+
+// export const User = (state={},action)=>{
+
+//     switch(action.type)
+//     {
+//         case REQUEST_LOAD_USER:
+//         case REQUEST_USER_LOGIN:
+//         case REQUEST_LOGOUT_USER:
+//             return{
+//                 loading:true,
+
+//             };
+//         case SUCCESS_LOAD_USER:
+//         case SUCCESS_USER_LOGIN:
+//         case SUCCESS_LOGOUT_USER:
+//             return{
+//                 loading:false,
+//                 isAuth:action.payload.authStatus,
+//                 user:action.payload.user,
+//                 message:action.payload.message
+//             }
+//         case FAIL_LOAD_USER: 
+//         case FAIL_USER_LOGIN:
+//         case FAIL_LOGOUT_USER: 
+//             return{
+//                 loading:false,
+//                 isAuth:false,
+//                 user:null,
+//                 message:action.payload
+
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
-export const getAllUsers = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case REQUEST_ALL_USER:
-            return{
-                loading:true,
 
-            };
 
-        case SUCCESS_ALL_USER:
-            return{
-                loading:false,
-               getUserStatus:action.payload.getUserStatus,
-               users:action.payload.users
-            }
-        case FAIL_ALL_USER: 
-            return{
-                loading:false,
-               getUserStatus:action.payload.getUserStatus,
-               users:action.payload.users,
-               message:action.payload.message
+// export const getAllUsers = (state={},action)=>{
 
-            }
-        default:
-            return state
-    }
-}
+//     switch(action.type)
+//     {
+//         case REQUEST_ALL_USER:
+//             return{
+//                 loading:true,
+
+//             };
+
+//         case SUCCESS_ALL_USER:
+//             return{
+//                 loading:false,
+//                getUserStatus:action.payload.getUserStatus,
+//                users:action.payload.users
+//             }
+//         case FAIL_ALL_USER: 
+//             return{
+//                 loading:false,
+//                getUserStatus:action.payload.getUserStatus,
+//                users:action.payload.users,
+//                message:action.payload.message
+
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
 
