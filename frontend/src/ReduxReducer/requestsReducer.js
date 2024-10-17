fb162f2605f0e3a8b32804aa9faba52f1215ee8a
Harshit Kumar <83143382+HarshitKumar1124@users.noreply.github.com>
2024-10-17 15:15:28 +0530

    Update requestsReducer.js Commented

--- frontend/src/ReduxReducer/requestsReducer.js
+++ frontend/src/ReduxReducer/requestsReducer.js
@@ -1,141 +1,141 @@
-import {
+// import {
     
-    REQUEST_GET_CONNECTION_REQUEST,
-    SUCCESS_GET_CONNECTION_REQUEST,
-    FAIL_GET_CONNECTION_REQUEST,
-    REQUEST_SEND_FRIEND_REQUEST,
-    SUCCESS_SEND_FRIEND_REQUEST,
-    FAIL_SEND_FRIEND_REQUEST,
-    REQUEST_ACCEPT_REQUEST,
-    SUCCESS_ACCEPT_REQUEST,
-    FAIL_ACCEPT_REQUEST,
-    REQUEST_DELETE_REQUEST,
-    SUCCESS_DELETE_REQUEST,
-    FAIL_DELETE_REQUEST,
+//     REQUEST_GET_CONNECTION_REQUEST,
+//     SUCCESS_GET_CONNECTION_REQUEST,
+//     FAIL_GET_CONNECTION_REQUEST,
+//     REQUEST_SEND_FRIEND_REQUEST,
+//     SUCCESS_SEND_FRIEND_REQUEST,
+//     FAIL_SEND_FRIEND_REQUEST,
+//     REQUEST_ACCEPT_REQUEST,
+//     SUCCESS_ACCEPT_REQUEST,
+//     FAIL_ACCEPT_REQUEST,
+//     REQUEST_DELETE_REQUEST,
+//     SUCCESS_DELETE_REQUEST,
+//     FAIL_DELETE_REQUEST,
    
 
-} from "../ReduxConstants/requestConstants"
+// } from "../ReduxConstants/requestConstants"
 
 
 
-/*Send Connection Requests */
+// /*Send Connection Requests */
 
-export const sendFriendRequest = (state={},action)=>{
+// export const sendFriendRequest = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case  REQUEST_SEND_FRIEND_REQUEST:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case  REQUEST_SEND_FRIEND_REQUEST:
+//             return{
+//                 loading:true,
 
-            };
-        case SUCCESS_SEND_FRIEND_REQUEST:
-            return{
-                loading:false,
-               requestStatus:action.payload.requestStatus,
-                message:action.payload.message
-            }
-        case FAIL_SEND_FRIEND_REQUEST: 
-            return{
-                loading:false,
-                requestStatus:action.payload.requestStatus,
-                message:action.payload.message
+//             };
+//         case SUCCESS_SEND_FRIEND_REQUEST:
+//             return{
+//                 loading:false,
+//                requestStatus:action.payload.requestStatus,
+//                 message:action.payload.message
+//             }
+//         case FAIL_SEND_FRIEND_REQUEST: 
+//             return{
+//                 loading:false,
+//                 requestStatus:action.payload.requestStatus,
+//                 message:action.payload.message
 
-            }
-        default:
-            return state
-    }
-}
-
-
-/*Get Connection Requests */
-
-export const getConnectionRequests = (state={},action)=>{
-
-    switch(action.type)
-    {
-        case REQUEST_GET_CONNECTION_REQUEST:
-            return{
-                loading:true,
-
-            };
-        case SUCCESS_GET_CONNECTION_REQUEST:
-            return{
-                loading:false,
-                fetched:action.payload.getSendRequestsStatus,
-                requests:action.payload.requests,
-                getRequestsStatus:true,
-                message:"Fetched User's Connection Request!"
-            }
-        case FAIL_GET_CONNECTION_REQUEST: 
-            return{
-                loading:false,
-                fetched:false,
-                requests:[],
-                getRequestsStatus:true,
-                message:action.payload
-
-            }
-        default:
-            return state
-    }
-}
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
+// /*Get Connection Requests */
 
-export const acceptRequest = (state={},action)=>{
+// export const getConnectionRequests = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case REQUEST_ACCEPT_REQUEST:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case REQUEST_GET_CONNECTION_REQUEST:
+//             return{
+//                 loading:true,
 
-            };
-        case SUCCESS_ACCEPT_REQUEST:
-            return{
-                loading:false,
-                acceptRequest:action.payload.acceptRequest,
-                message:action.payload.message
-            }
-        case FAIL_ACCEPT_REQUEST: 
-            return{
-                loading:false,
-                acceptRequest:action.payload.acceptRequest,
-                message:action.payload
+//             };
+//         case SUCCESS_GET_CONNECTION_REQUEST:
+//             return{
+//                 loading:false,
+//                 fetched:action.payload.getSendRequestsStatus,
+//                 requests:action.payload.requests,
+//                 getRequestsStatus:true,
+//                 message:"Fetched User's Connection Request!"
+//             }
+//         case FAIL_GET_CONNECTION_REQUEST: 
+//             return{
+//                 loading:false,
+//                 fetched:false,
+//                 requests:[],
+//                 getRequestsStatus:true,
+//                 message:action.payload
 
-            }
-        default:
-            return state
-    }
-}
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
 
-export const deleteRequest = (state={},action)=>{
+// export const acceptRequest = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case REQUEST_DELETE_REQUEST:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case REQUEST_ACCEPT_REQUEST:
+//             return{
+//                 loading:true,
 
-            };
-        case SUCCESS_DELETE_REQUEST:
-            return{
-                loading:false,
-                deleteRequest:action.payload.deleteRequest,
-                message:action.payload.message
-            }
-        case FAIL_DELETE_REQUEST: 
-            return{
-                loading:false,
-                deleteRequest:action.payload.deleteRequest,
-                message:action.payload
-            }
-        default:
-            return state
-    }
-}
+//             };
+//         case SUCCESS_ACCEPT_REQUEST:
+//             return{
+//                 loading:false,
+//                 acceptRequest:action.payload.acceptRequest,
+//                 message:action.payload.message
+//             }
+//         case FAIL_ACCEPT_REQUEST: 
+//             return{
+//                 loading:false,
+//                 acceptRequest:action.payload.acceptRequest,
+//                 message:action.payload
+
+//             }
+//         default:
+//             return state
+//     }
+// }
+
+
+
+// export const deleteRequest = (state={},action)=>{
+
+//     switch(action.type)
+//     {
+//         case REQUEST_DELETE_REQUEST:
+//             return{
+//                 loading:true,
+
+//             };
+//         case SUCCESS_DELETE_REQUEST:
+//             return{
+//                 loading:false,
+//                 deleteRequest:action.payload.deleteRequest,
+//                 message:action.payload.message
+//             }
+//         case FAIL_DELETE_REQUEST: 
+//             return{
+//                 loading:false,
+//                 deleteRequest:action.payload.deleteRequest,
+//                 message:action.payload
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
