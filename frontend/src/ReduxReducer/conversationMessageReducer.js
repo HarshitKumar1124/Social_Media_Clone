eb0af7738bd022fc507b949e3d187bec3227386c
Harshit Kumar <83143382+HarshitKumar1124@users.noreply.github.com>
2024-10-17 15:15:02 +0530

    Update Commented  conversationMessageReducer.js

--- frontend/src/ReduxReducer/conversationMessageReducer.js
+++ frontend/src/ReduxReducer/conversationMessageReducer.js
@@ -1,142 +1,142 @@
-import {
+// import {
 
-    REQUEST_GET_ALL_CONVERSATIONS,
-    SUCCESS_GET_ALL_CONVERSATIONS,
-    FAIL_GET_ALL_CONVERSATIONS,
-    REQUEST_GET_CHAT_MESSAGES,
-    SUCCESS_GET_CHAT_MESSAGES,
-    FAIL_GET_CHAT_MESSAGES,
-    REQUEST_CREATE_CHAT_MESSAGES,
-    SUCCESS_CREATE_CHAT_MESSAGES,
-    FAIL_CREATE_CHAT_MESSAGES,
-    DELETE_MESSAGE_FAIL,
-    DELETE_MESSAGE_REQUEST,
-    DELETE_MESSAGE_SUCCESS
+//     REQUEST_GET_ALL_CONVERSATIONS,
+//     SUCCESS_GET_ALL_CONVERSATIONS,
+//     FAIL_GET_ALL_CONVERSATIONS,
+//     REQUEST_GET_CHAT_MESSAGES,
+//     SUCCESS_GET_CHAT_MESSAGES,
+//     FAIL_GET_CHAT_MESSAGES,
+//     REQUEST_CREATE_CHAT_MESSAGES,
+//     SUCCESS_CREATE_CHAT_MESSAGES,
+//     FAIL_CREATE_CHAT_MESSAGES,
+//     DELETE_MESSAGE_FAIL,
+//     DELETE_MESSAGE_REQUEST,
+//     DELETE_MESSAGE_SUCCESS
    
 
-} from "../ReduxConstants/conversationMessageConstants"
+// } from "../ReduxConstants/conversationMessageConstants"
 
 
 
-// sendMessage
+// // sendMessage
 
 
-export const sendMessage = (state={},action)=>{
+// export const sendMessage = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case REQUEST_CREATE_CHAT_MESSAGES:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case REQUEST_CREATE_CHAT_MESSAGES:
+//             return{
+//                 loading:true,
 
-            };
-        case SUCCESS_CREATE_CHAT_MESSAGES:
-            return{
-                loading:false,
-                createMessageStatus:action.payload
+//             };
+//         case SUCCESS_CREATE_CHAT_MESSAGES:
+//             return{
+//                 loading:false,
+//                 createMessageStatus:action.payload
                 
-            }
-        case FAIL_CREATE_CHAT_MESSAGES: 
-            return{
-                loading:false,
-                createMessageStatus:action.payload
+//             }
+//         case FAIL_CREATE_CHAT_MESSAGES: 
+//             return{
+//                 loading:false,
+//                 createMessageStatus:action.payload
                 
-            }
-        default:
-            return state
-    }
-}
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
 
-export const getConversations = (state={},action)=>{
+// export const getConversations = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case REQUEST_GET_ALL_CONVERSATIONS:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case REQUEST_GET_ALL_CONVERSATIONS:
+//             return{
+//                 loading:true,
 
-            };
-        case SUCCESS_GET_ALL_CONVERSATIONS:
-            return{
-                loading:false,
-                getConversationStatus:action.payload.getConversationStatus,
-                AllConversations:action.payload.AllConversations,
-                message:action.payload.message
+//             };
+//         case SUCCESS_GET_ALL_CONVERSATIONS:
+//             return{
+//                 loading:false,
+//                 getConversationStatus:action.payload.getConversationStatus,
+//                 AllConversations:action.payload.AllConversations,
+//                 message:action.payload.message
                 
-            }
-        case FAIL_GET_ALL_CONVERSATIONS: 
-            return{
-                loading:false,
-                getConversationStatus:action.payload.getConversationStatus,
-                message:action.payload.message,
-                AllConversations:[]
+//             }
+//         case FAIL_GET_ALL_CONVERSATIONS: 
+//             return{
+//                 loading:false,
+//                 getConversationStatus:action.payload.getConversationStatus,
+//                 message:action.payload.message,
+//                 AllConversations:[]
                 
-            }
-        default:
-            return state
-    }
-}
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
 
-export const getChat = (state={},action)=>{
+// export const getChat = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case REQUEST_GET_CHAT_MESSAGES:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case REQUEST_GET_CHAT_MESSAGES:
+//             return{
+//                 loading:true,
 
-            };
-        case SUCCESS_GET_CHAT_MESSAGES:
-            return{
-                loading:false,
-                getChatsStatus:action.payload.getChatsStatus,
-                chats:action.payload.chats,
-                target_id:action.payload.target_id,
-                message:action.payload.message,
-                friendStatus:action.payload.friendStatus,
+//             };
+//         case SUCCESS_GET_CHAT_MESSAGES:
+//             return{
+//                 loading:false,
+//                 getChatsStatus:action.payload.getChatsStatus,
+//                 chats:action.payload.chats,
+//                 target_id:action.payload.target_id,
+//                 message:action.payload.message,
+//                 friendStatus:action.payload.friendStatus,
                 
-            }
-        case FAIL_GET_CHAT_MESSAGES: 
-            return{
-                loading:false,
-                getChatsStatus:action.payload.getChatsStatus,
-                chats:null,
-                message:action.payload,
+//             }
+//         case FAIL_GET_CHAT_MESSAGES: 
+//             return{
+//                 loading:false,
+//                 getChatsStatus:action.payload.getChatsStatus,
+//                 chats:null,
+//                 message:action.payload,
                 
-            }
-        default:
-            return state
-    }
-}
+//             }
+//         default:
+//             return state
+//     }
+// }
 
 
 
-export const deleteMessage = (state={},action)=>{
+// export const deleteMessage = (state={},action)=>{
 
-    switch(action.type)
-    {
-        case DELETE_MESSAGE_REQUEST:
-            return{
-                loading:true,
+//     switch(action.type)
+//     {
+//         case DELETE_MESSAGE_REQUEST:
+//             return{
+//                 loading:true,
 
-            };
-        case DELETE_MESSAGE_SUCCESS:
-            return{
-                loading:false,
-                deleteStatus:true
+//             };
+//         case DELETE_MESSAGE_SUCCESS:
+//             return{
+//                 loading:false,
+//                 deleteStatus:true
                 
-            }
-        case DELETE_MESSAGE_FAIL: 
-            return{
-                loading:false,
-                deleteStatus:false
+//             }
+//         case DELETE_MESSAGE_FAIL: 
+//             return{
+//                 loading:false,
+//                 deleteStatus:false
                 
-            }
-        default:
-            return state
-    }
-}
+//             }
+//         default:
+//             return state
+//     }
+// }
